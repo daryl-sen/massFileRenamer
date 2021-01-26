83c435355097a362a1433551f2b8fc4d222eb5ee
daryl <tsdaryl@gmail.com>
2021-01-26 12:15:08 -0800

    tested and ready to use

--- fileRenamer.js
+++ fileRenamer.js
@@ -1,9 +1,9 @@
 // alter to your needs
-const targetFolderName = 'testfolder';
-const toReplace = 'a';
-const replaceWith = ''; // leave as empty string to remove a specific string
-const prefix = 'pre';
-const suffix = 'suff';
+const targetFolderName = 'testfolder'; // the name of the folder RELATIVE to this directory
+const toReplace = ''; // the undesired part of the name
+const replaceWith = ''; // what to replace the undesired part with; leave as empty string to just remove it
+const prefix = ''; // add in front of the file name
+const suffix = ''; // add behind the file name
 
 // do not alter
 const path = require('path')
@@ -11,16 +11,15 @@ const fs = require('fs');
 const rootFolder = process.cwd();
 const targetFolderPath = path.join(rootFolder, targetFolderName);
 
+// find all the files in the target directory
 fs.readdirSync(targetFolderPath).forEach(file => {
-  const fileString = file.split('.')
-  const fileName = fileString[0]
-  const fileExt = fileString[1]
-
+  const fileString = file.split('.');
+  const fileName = fileString[0];
+  const fileExt = fileString[1];
   const newFileName = prefix + fileName.replace(toReplace, replaceWith) + suffix;
   const newFilePath = path.join(targetFolderPath, `${newFileName}.${fileExt}`);
 
-  // console.log(file);
-
+  // rename the file
   fs.rename(path.join(targetFolderPath, file), newFilePath, function(err) {
     if ( err ) console.log('ERROR: ' + err);
   });
